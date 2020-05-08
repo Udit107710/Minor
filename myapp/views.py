d3d7388a02da1b851cb130b6ce90470f0fcf6524
Udit107710 <uditcr710107@gmail.com>
2020-05-08 21:35:57 +0530

    Project completed

--- myapp/views.py
+++ myapp/views.py
@@ -1,43 +1,19 @@
-from django.shortcuts import render, HttpResponse
-from sklearn.externals import joblib
-import numpy as np
-import pandas as pd
-from sklearn import datasets
-from .models import Results
-from .forms import PredictForm
-import os
+from django.views.generic.edit import FormView
+from .forms import FileFieldForm
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-file_name = dir_path + "/iris_svm_model.pkl"
-
-def predict(requests):
-	#return render(request, 'myapp/predict_form.html')
-	if requests.method == 'POST' :
-		my_form = PredictForm(requests.POST)
-		if my_form.is_valid():
-			petal_length = my_form.cleaned_data['petal_length']
-			petal_width = my_form.cleaned_data['petal_width']
-			sepal_length = my_form.cleaned_data['sepal_length']
-			sepal_width = my_form.cleaned_data['sepal_width'] 
-			
-			iris = datasets.load_iris()
-			target = iris.target_names
-
-			val = np.array([petal_length, petal_width,sepal_length,sepal_width])
-			model = joblib.load(file_name)
-			y_pred = model.predict(val.reshape(1,-1))
-			prediction = target[y_pred[0]]
-			#result = Results(petal_length= petal_length, petal_width=petal_width, sepal_length=sepal_length, sepal_width-sepal_width, prediction=prediction)
-			#results.save()
-			result = my_form.save(commit= False)
-			result.prediction = prediction 
-			result.save()
-			return render(requests, 'myapp/result.html',{'prediction': prediction} )
+class FileFieldView(FormView):
+	form_class = FileFieldForm
+	template_name = 'myapp/predict_form.html'  # Replace with your template.
+	success_url = 'myapp/result.html'  # Replace with your URL or reverse().
+	
+	def post(self, request, *args, **kwargs):
+		form_class = self.get_form_class()
+		form = self.get_form(form_class)
+		files = request.FILES.getlist('file_field')
+		if form.is_valid():
+			for f in files:
+				# Do something with each file.
+				print("hello")
+			return self.form_valid(form)
 		else:
-			return HttpResponse( my_form.errors)
-	else:
-		my_form = PredictForm()
-		return render(requests, 'myapp/predict_form.html', {'form': my_form})
-
-
-			
+			return self.form_invalid(form)
--- myproject/settings.py
+++ myproject/settings.py
@@ -74,15 +74,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-	'USER': 'postgres',
-	'HOST': 'db',
-	'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+# 	'USER': 'postgres',
+# 	'HOST': 'db',
+# 	'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
